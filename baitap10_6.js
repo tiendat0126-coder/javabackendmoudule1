a8687873aa5a3a7d91cda2014ae928059a9eb6d3
tiendat0126-coder <tiendat0126@gmail.com>
2026-02-04 20:51:40 +0900

    Module1_Session 10-6

---
+++ baitap10_6.js
@@ -0,0 +1,5 @@
+let mathPoint = Number(prompt("Nhập điểm Toán:"));
+let physicsPoint = Number(prompt("Nhập điểm Vật lý:"));
+let chemistryPoint = Number(prompt("Nhập điểm Hóa:"));
+const average = (mathPoint + physicsPoint + chemistryPoint) / 3;
+console.log("Điểm trung bình 3 môn học là:", average);
